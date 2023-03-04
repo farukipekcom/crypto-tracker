17845df449bb4d831417772c24da6dd34da66a03
Faruk İpek <farukipekweb@gmail.com>
2023-03-03 21:21:43 -0500

    updated api

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,47 +4,49 @@ import { UpIcon, DownIcon } from "../assets/icons";
 function ListItem({ crypto }) {
   return (
     <>
-      {crypto.map((coin) =>
-        coin.market_data.price_change_percentage_24h > 0 ? (
-          <Link to={`/crypto/${coin.id}`} key={coin.id}>
-            <div className="card up-shadow">
-              <div className="header">
-                <img src={coin.image.small} alt={coin.image.name} />
-                <span className="up">
-                  <UpIcon />
-                  <span className="price">
-                    {Number(
-                      coin.market_data.price_change_percentage_24h
-                    ).toFixed(2)}
-                    %
+      {
+        crypto.map((coin) =>
+          coin.price_change_percentage_24h > 0 ? (
+            <Link to={`/crypto/${coin.id}`} key={coin.id}>
+              <div className="card up-shadow">
+                <div className="header">
+                  <img src={coin.image} alt={coin.image.name} />
+                  <span className="up">
+                    <UpIcon />
+                    <span className="price">
+                      {Number(
+                        coin.price_change_percentage_24h
+                      ).toFixed(2)}
+                      %
+                    </span>
                   </span>
-                </span>
+                </div>
+                <h1 className="coin-title">{coin.name}</h1>
+                <p className="price">${coin.current_price}</p>
               </div>
-              <h1 className="coin-title">{coin.name}</h1>
-              <p className="price">${coin.market_data.current_price.usd}</p>
-            </div>
-          </Link>
-        ) : (
-          <Link to={`/crypto/${coin.id}`} key={coin.id}>
-            <div className="card down-shadow" key={coin.id}>
-              <div className="header">
-                <img src={coin.image.small} alt={coin.image.name} />
-                <span className="down">
-                  <DownIcon />
-                  <span className="price">
-                    {Number(
-                      coin.market_data.price_change_percentage_24h
-                    ).toFixed(2)}
-                    %
+            </Link>
+          ) : (
+            <Link to={`/crypto/${coin.id}`} key={coin.id}>
+              <div className="card down-shadow" key={coin.id}>
+                <div className="header">
+                  <img src={coin.image} alt={coin.image.name} />
+                  <span className="down">
+                    <DownIcon />
+                    <span className="price">
+                      {Number(
+                        coin.price_change_percentage_24h
+                      ).toFixed(2)}
+                      %
+                    </span>
                   </span>
-                </span>
+                </div>
+                <h1 className="coin-title">{coin.name}</h1>
+                <p className="price">${coin.current_price}</p>
               </div>
-              <h1 className="coin-title">{coin.name}</h1>
-              <p className="price">${coin.market_data.current_price.usd}</p>
-            </div>
-          </Link>
+            </Link>
+          )
         )
-      )}
+      }
     </>
   );
 }
--- src/redux/cryptoSlice.js
+++ src/redux/cryptoSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchCrypto = createAsyncThunk("crypto/getCrypto", async () => {
-  const res = await axios("https://api.coingecko.com/api/v3/coins");
+  const res = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Cmatic-network%2Cdogecoin%2Csolana%2Cpolkadot%2Clitecoin%2Ctron%2Cavalanche-2%2Cuniswap%2Cchainlink%2Ccosmos%2Cethereum-classic&order=market_cap_desc&per_page=100&page=1&sparkline=false");
   return res.data;
 });
 
